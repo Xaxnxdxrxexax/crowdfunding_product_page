c4d122fcce99993411ce866cc7fdc32ad76cad7a
Andrea Sansone <andreasansone35@gmail.com>
2023-10-16 11:45:39 +0200

    header done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,68 @@
-import Link from "next/link";
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <main className="">
+      <header className="relative flex items-center">
+        <picture className="">
+          <source
+            srcSet="/images/image-hero-desktop.jpg"
+            media="(min-width: 376px)"
+          />
+          <Image
+            src={"/images/image-hero-mobile.jpg"}
+            alt="hero"
+            width={375}
+            height={300}
+            className="w-full"
+          />
+        </picture>
+        <div
+          className={`${
+            isHamburgerOpen ? "block" : "hidden"
+          } fixed left-0 top-0 h-screen w-screen bg-gradient-to-b from-[rgba(0,0,0,0.35)] to-[rgba(0,0,0,0.05)]`}
+        ></div>
+        <nav className="Fm:w-[78%] Fm:ml-40 Fm:mt-11 absolute left-0 top-0 mx-5 mt-8 flex w-[90%] items-center">
+          <Image
+            src={"/images/logo.svg"}
+            alt="logo"
+            width="128"
+            height="20"
+            className=""
+          />
+          <Image
+            src={
+              isHamburgerOpen
+                ? "/images/icon-close-menu.svg"
+                : "/images/icon-hamburger.svg"
+            }
+            alt="hamburger"
+            width={isHamburgerOpen ? "14" : "16"}
+            height="15"
+            className="Fm:invisible Fm:absolute Fm:top-0 z-10 ml-auto"
+            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
+          />
+          {isHamburgerOpen && (
+            <div className="absolute top-10 z-10 mx-auto w-full rounded-2xl bg-white">
+              <ul className="mx-auto flex w-[90%] flex-col items-start gap-10 p-6 px-2 text-lg">
+                {["About", "Discover", "Get Started"].map((item) => (
+                  <li key={item} className="hover:cursor-pointer">
+                    {item}
+                  </li>
+                ))}
+              </ul>
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+          )}
+          <ul className="Fm:visible Fm:static invisible absolute ml-auto flex items-center gap-8 font-medium text-white">
+            {["About", "Discover", "Get Started"].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </nav>
+      </header>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      screens: {
+        Fm: "376px",
+      },
+      colors: {
+        "Fm-Moderate-cyan": "hsl(176, 50%, 47%)",
+        "Fm-Dark-cyan": "hsl(176, 72%, 28%)",
+        "Fm-Black": "hsl(0, 0%, 0%)",
+        "Fm-Dark-gray": "hsl(0, 0%, 48%)",
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
